f13f5fc6e64b47f898ae9d84f606c59f84c5b540
Marcel Guzik <mguzik555@gmail.com>
2019-09-17 21:25:07 +0200

    Added changing modes

--- main.py
+++ main.py
@@ -3,16 +3,49 @@
 import serial
 import sys
 
+
+def set_brightness(args):
+    brightness = bytes.fromhex(args.pop(0))
+    if len(brightness) > 1:
+        print('brightness should be in range 00-ff')
+        sys.exit(2)
+    return b'\x02' + brightness
+
+
+def enable(args):
+    return b'\x01\xff'
+
+
+def disable(args):
+    return b'\x01\x00'
+
+
+def set_mode(args):
+    mode = args.pop(0)
+    payload = b'\x04'
+    if mode == 'capture':
+        return payload + b'\xff'
+    elif mode == 'ambient':
+        return payload + b'\x00'
+    else:
+        return b''
+
+
 payloads = {
-    'enable': b'\x01\xff',
-    'disable': b'\x01\x00'
+    'enable': enable,
+    'disable': disable,
+    'brightness': set_brightness,
+    'mode': set_mode
 }
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0')
-    print(ser.name)
+    ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+    if len(sys.argv) < 2:
+        print('Usage: lightctl command [args]')
+        sys.exit(1)
 
     command, *args = sys.argv[1:]
 
-    payload = payloads[command]
+    payload = payloads[command](args)
     ser.write(payload)
